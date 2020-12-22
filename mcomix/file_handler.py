dfc9dd5c33c4bd30cb2470b9311e30a68ec1ab9b
completesomething <76411090+completesomething@users.noreply.github.com>
2020-12-22 10:29:51 -0500

    Fix certain case with first file in arch/dir preferences
    If the user had 'first file in prev arch' enabled but 'first file in prev dir' disabled, going to the previous archive located in a previous directory would open the last file in that archive, which is undesirable with the enabled behavior. This should fix that case.

--- mcomix/file_handler.py
+++ mcomix/file_handler.py
@@ -550,10 +550,11 @@ class FileHandler(object):
         else:
             path = self._file_provider.get_directory()
 
-        if prefs['open first file in prev directory']:
+        if prefs['open first file in prev archive']:
             self.open_file(path, 0, keep_fileprovider=True)
         else:
-            self.open_file(path, -1, keep_fileprovider=True)
+            self.open_file(path, prefs['open first file in prev directory']-1,
+                            keep_fileprovider=True)
         return True
 
     def file_is_available(self, filepath):
